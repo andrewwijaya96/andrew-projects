c92bd6fec7d4f9f1e2ce136990fb9407261d3f0e
Andrew Wijaya <33770251+andrewwijaya96@users.noreply.github.com>
2024-11-11 15:09:45 +0700

    not null

--- bytebase/6_testembusprod.sql
+++ bytebase/6_testembusprod.sql
@@ -1,8 +1,8 @@
 CREATE TABLE testkeprod_table (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(100),
-    age INTEGER,
-    email VARCHAR(255)
+    name VARCHAR(100) NOT NULL,
+    age INTEGER NOT NULL,
+    email VARCHAR(255) NOT NULL
 );
 
 INSERT INTO testkeprod_table (name, age, email)
